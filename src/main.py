b8cf64509c8b4b1ba86d04a41280fe6a44aef2d9
Murillo Alves <murilloalveslou@outlook.com>
2026-07-08 10:40:30 -0300

    refactor: organize code into reusable functions

--- src/main.py
+++ src/main.py
@@ -1,33 +1,57 @@
 import pandas as pd
 
-dados = pd.read_csv("data/gastos.csv")
+def carregar_dados():
+    dados = pd.read_csv("data/gastos.csv")
+    
+    return dados
 
-gastos = dados[dados["Valor"] < 0]
+def calcular_resumo(dados):
+    gastos = dados[dados["Valor"] < 0]
+    receitas = dados[dados["Valor"] > 0]
+    total_receitas = receitas["Valor"].sum()
+    total_gastos = gastos["Valor"].abs().sum()
+    saldo = total_receitas - total_gastos
+    
+    return gastos, receitas, total_receitas, total_gastos, saldo 
 
-receitas = dados[dados["Valor"] > 0]
+def analisar_categorias(gastos):
+    gastos_categoria = (
+        gastos.groupby("Categoria")["Valor"]
+        .sum()
+        .sort_values()
+        )
+    
+    return gastos_categoria 
 
-print("\nReceitas:")
-print(receitas)
+def main():
+    dados = carregar_dados()
 
-print("\nGastos:")
-print(gastos)
+    gastos, receitas, total_receitas, total_gastos, saldo  = calcular_resumo(dados)
 
-total_receitas = receitas["Valor"].sum()
+    gastos_categoria = analisar_categorias(gastos)
+    
+    print("\nReceitas:")
+    print(receitas)
 
-total_gastos = gastos["Valor"].abs().sum()
+    print("\nGastos:")
+    print(gastos)
 
-saldo = total_receitas - total_gastos
+    print(f"\nTotal de Receitas: R${total_receitas:.2f}")
+    print(f"\nTotal de Gastos: R${total_gastos:.2f}")
+    print(f"\nSaldo: R$ {saldo:.2f}")
 
-print("\nTotal de receitas:", total_receitas)
-print("Total de gastos:", total_gastos)
-print("Saldo:", saldo)
+    print("\nGastos por categoria:")
+    print(gastos_categoria)
 
-gastos_categoria = (
-    gastos.groupby("Categoria")["Valor"]
-    .sum()
-    .sort_values()
-    )
 
-print(gastos_categoria)
+if __name__ == "__main__":
+    main()
+   
+
+
+
+
+
+
 
 
